9bc039941a42bf5176794df32e51af2aed792329
dcomtois <dominic.comtois@gmail.com>
2015-04-03 13:19:05 -0400

    added more object info in what.is - object class type (S3, S4, RC), methods related to classes of the object or methods for generic functions.

--- R/what.is.R
+++ R/what.is.R
@@ -1,6 +1,6 @@
 what.is <- function(x, show.all=FALSE, ignore.size.warn=FALSE) {
 
-  if(object.size(x) > 20000 && ignore.size.warn == FALSE) {
+  if(!is.function(x) && object.size(x) > 20000 && ignore.size.warn == FALSE) {
     stop("object.size(x) is greater than 10K; computing time might be long. Set argument ignore.size.warn to TRUE to force execution anyway")
   }
   # set the warn option to -1 to temporarily ignore warnings
@@ -9,30 +9,36 @@ what.is <- function(x, show.all=FALSE, ignore.size.warn=FALSE) {
   on.exit(options(op))
 
   # Part 1. Data Properties - class, typeof, mode, storage.mode
-  obj.properties <- data.frame(property=c("class", "typeof", "mode", "storage.mode", "dim", "length", "object.size"),
+  properties <- data.frame(property=c("class", "typeof", "mode", "storage.mode", "dim", "length", "object.size"),
                                value = c(paste(class(x),collapse=" "), typeof(x), mode(x), storage.mode(x),
                                          paste(dim(x), collapse = " x "), length(x), paste(object.size(x), "Bytes")))
 
 
-  # Part 2. Test object against all "is.[...]" functions
+  # Part 2. Make a list of all x's attribute and their length
+  attributes.lengths <- sapply(attributes(x),length)
+  if(length(attributes.lengths)==0)
+    attributes.lengths <- NULL
+
+
+  # Part 3. Test object against all "is.[...]" functions
   # Look for all relevant functions
   list.id.fun <- grep(methods(is), pattern = "<-", invert = TRUE, value = TRUE)
 
   # remove is.R which is not relevant and can take a lot of time
   list.id.fun <- setdiff(list.id.fun, "is.R")
 
-  # loop over all functions with x as argument, and store the results
+  # loop over all "is" functions with x as argument, and store the results
   if(!show.all) {
-    identifiers <- c()
+    extensive.is <- c()
     for(fun in list.id.fun) {
       res <- try(eval(call(fun,x)),silent=TRUE)
       if(isTRUE(res))
-        identifiers <- append(identifiers, fun)
+        extensive.is <- append(extensive.is, fun)
     }
   } else {
 
     # Generate table of all identifier tests
-    identifiers <- data.frame(test=character(), value=character(),
+    extensive.is <- data.frame(test=character(), value=character(),
                                    warnings=character(), stringsAsFactors = FALSE)
 
     # loop over all functions with x as argument, and store the results
@@ -46,21 +52,41 @@ what.is <- function(x, show.all=FALSE, ignore.size.warn=FALSE) {
       } else {
         warn <- ""
       }
-      identifiers[nrow(identifiers)+1,] <- list(fun, value, warn)
+      extensive.is[nrow(extensive.is)+1,] <- list(fun, value, warn)
     }
 
     # sort the results according to the results and warnings if any
-    identifiers <- identifiers[order(identifiers$value, identifiers$warnings=="", decreasing = TRUE),]
+    extensive.is <- extensive.is[order(extensive.is$value, extensive.is$warnings=="", decreasing = TRUE),]
   }
 
-  # Part 3. Make a list of all x's attribute and their length
-  obj.attributes <- sapply(attributes(x),length)
+  # Part 4. Get info on the type of object - S3, S4, attributes / slots
 
+  function.type <- NULL
+	generic.function.methods <- NULL
+	object.type <- NULL
+	obj.class.methods <- NULL
+
+  if(is.function(x)) {
+
+  	function.type <- pryr::ftype(x)
+  	if("generic" %in% function.type)
+  		generic.function.methods <- methods(deparse(substitute(x)))
+
+  } else {
+
+  	object.type <- pryr::otype(x)
+  	obj.class.methods <- lapply(class(x), function(cl) methods(class=cl))
+  	names(obj.class.methods) <- class(x)
+  }
 
   output <- list()
-  output$obj.properties <- obj.properties
-  output$identifiers <- identifiers
-  output$attributes <- obj.attributes
+  output$properties <- properties
+  output$attributes.lengths <- attributes.lengths
+  output$extensive.is <- extensive.is
+  output$function.type <- function.type
+  output$generic.function.methods <- generic.function.methods
+  output$object.type <- object.type
+  output$obj.class.methods <- obj.class.methods
   return(output)
 
 }
