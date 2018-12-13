49e8a7893eddc2bbbc4c021756cffe207efdddfb
Dominic Comtois <dominic.comtois@gmail.com>
2018-12-13 00:04:09 -0500

    refine names gathering

--- R/parse_args.R
+++ R/parse_args.R
@@ -185,17 +185,17 @@ parse_args <- function(sys_calls, sys_frames, match_call,
         break
       for (obj.name in allnames) {
         if (exists(obj.name, envir = sys_frames[[no.frame]], mode = "list")) {
-          df <- get(obj.name, envir = sys_frames[[no.frame]])
-          if (is.data.frame(df)) {
+          df_ <- get(obj.name, envir = sys_frames[[no.frame]])
+          if (is.data.frame(df_)) {
             df_name   <- obj.name
             df_pos    <- no.frame
-            df_label  <- label(df)
+            df_label  <- label(df_)
             # Find colnames
             if (max.varnames > 0) {
-              col_names <- colnames(df)
+              col_names <- colnames(df_)
               if (length(intersect(allnames, col_names)) > 0) {
                 var_names <- head(intersect(allnames, col_names), max.varnames)
-                var_label <- label(df[var_names[1]])
+                var_label <- label(df_[var_names[1]])
               } else {
                 possible.iterator <- setdiff(allnames, df_name)
               }
@@ -207,7 +207,7 @@ parse_args <- function(sys_calls, sys_frames, match_call,
       }
     }
     
-    if (exists("df") && exists("possible.iterator")) {
+    if (exists("df_") && exists("possible.iterator")) {
       cont <- TRUE
       for (no.frame in seq_along(sys_frames)) {
         if (!cont) 
@@ -223,132 +223,47 @@ parse_args <- function(sys_calls, sys_frames, match_call,
         }
       }
     }
+    
+    # Look for a "stand-alone" variable
+    # look for numeric if function is descr()
+    if (length(var_names) == 0 && grepl("descr", deparse(match_call))) {
+      cont <- TRUE
+      for (no.frame in seq_along(sys_frames)) {
+        if (!cont) 
+          break
+        for (obj.name in allnames) {
+          if (exists(obj.name, envir = sys_frames[[no.frame]], 
+                     mode = "numeric")) {
+            var_names <- obj.name
+            var_label <- label(get(obj.name, envir = sys_frames[[no.frame]]))
+            cont <- FALSE
+            break
+          }
+        }
+      }
+    } else {
+      cont <- TRUE
+      for (no.frame in seq_along(sys_frames)) {
+        if (!cont) 
+          break
+        for (obj.name in allnames) {
+          if (exists(obj.name, envir = sys_frames[[no.frame]], 
+                     mode = "numeric") ||
+              exists(obj.name, envir = sys_frames[[no.frame]],
+                     mode = "character")) {
+            var_names <- obj.name
+            var_label <- label(get(obj.name, envir = sys_frames[[no.frame]]))
+            cont <- FALSE
+            break
+          }
+        }
+      }
+    }
+    
+    if (length(var_names) == 0 && length(allnames) == 1) {
+      var_names <- allnames
+    }
   }
-  
-  # From here code applies no matter how function was called -----------------
-  # We have missing information
-  # Extract call as a string
-  
-  # get names of objects in all available frames
-  # objects_in_frames <- list()
-  # for (no.frame in seq_along(sys_frames)) {
-  #   objects_in_frame <- character()
-  #   for (obj.number in seq_along(ls.str(sys.frames()[[no.frame]]))) {
-  #     objects_in_frame <- c(objects_in_frame, ls.str(sys.frames()[[no.frame]])[obj.number])
-  #   }
-  #   objects_in_frames[[no.frame]] <- objects_in_frame
-  # }
-  
-  # # regex to try and parse indexing
-  # re_indexing <- 
-  #   paste0(
-  #     "^([\\w\\.\\_]+)\\s*", # normally, data frame name (1)
-  #     "\\[(.+)?",            # rows indexing  (2)
-  #     "\\s*(\\,)\\s*",       # comma surrounded (or not) by spaces     (3)
-  #     "(.*?)",               # column indexing                         (4)
-  #     "\\s*\\]$")            # end of indexing
-  # 
-  # 
-  # # re2: dfname[[col]] (+ optionnal rows indexing)
-  # re_dbl_brackets <- 
-  #   paste0("([\\w\\.\\_]+)",                 # data frame               (1)
-  #          "\\s*\\[\\[\\s*(.*)\\s*\\]\\]",   # variable number or name  (2)
-  #          "(\\s*\\[\\s*(.*)\\s*\\])?")      # rows indexing (opt)      (4)
-  # 
-  
-  #     # Look for a data frame; go up the chain of environments
-  #     cont <- TRUE
-  #     for (i.name in seq_along(allnames)) {
-  #       if (!cont)
-  #         break
-  #       for (j.frame in seq_along(sys.frames())) {
-  #         check <- exists(x = allnames[i.name], where = j.frame, mode = 'list')
-  #         if (isTRUE(check)) {
-  #           obj <- get(allnames[i.name], pos = j.frame)
-  #           if (is.data.frame(obj)) {
-  #             df_name <- allnames[i.name]
-  #             df_label <- label(obj)
-  #             df_frame_pos <- j.frame
-  #             if (length(allnames) == 1 && max.varnames != Inf) {
-  #               # if there is column indexing, simplify it so to get df[col]
-  #               if (grepl(re_indexing, data_str, perl = TRUE)) {
-  #                 var_names <- 
-  #                   colnames(obj[as.numeric(sub(re_indexing, "\\4", 
-  #                                               x = data_str, perl = TRUE))])
-  #                 var_label <- 
-  #                   label(obj[eval(sub(re_indexing, "\\4", 
-  #                                      x = data_str, perl = TRUE))])
-  #                 cont <- FALSE
-  #                 break
-  #               } else if (grepl(re_dbl_brackets, data_str, perl = TRUE)) {
-  #                 # try to convert to numeric what's inside the dbl brackets
-  #                 tmp_content <- unquote(sub(re_dbl_brackets, "\\2", 
-  #                                            x = data_str, perl = TRUE))
-  #                 varnum <- suppressWarnings(as.numeric(tmp_content))
-  #                 if (is.na(varnum)) {
-  #                   var_names <- colnames(obj[tmp_content])
-  #                   var_label <- label(obj[tmp_content])
-  #                 } else {
-  #                   var_names <- colnames(obj[as.numeric(tmp_content)])
-  #                   var_label <- label(obj[as.numeric(tmp_content)])
-  #                 }
-  #                 cont <- FALSE
-  #                 break
-  #               }
-  #             } else {
-  #               # more than 1 name in allnames - see if these are variable names
-  #               tmp <- intersect(colnames(obj), allnames)
-  #               if (length(tmp) > 0) {
-  #                 # truncate the vector var_names to the max expected
-  #                 length(tmp) <- min(length(tmp), max.varnames) 
-  #                 var_names <- tmp
-  #                 var_label <- label(obj[,var_names])
-  #               }
-  #               cont <- FALSE
-  #               break
-  #             }
-  #           }
-  #         }
-  #       }
-  #     }
-  #     
-  #     # A dataframe was found, but not a variable;
-  #     # There might be an iterator accessible in the same frame as
-  #     # the dataframe (df_frame_pos)
-  #     if (length(df_name) == 1 && length(allnames) == 2) {
-  #       name = setdiff(allnames, df_name)
-  #       check <- exists(x = name, where = df_frame_pos, mode = 'numeric')
-  #       if (isTRUE(check)) {
-  #         # we have the value of the iterator; get the name of the variable
-  #         col_num <- get(name, pos = j.frame)
-  #         var_names <- colnames(obj[col_num])
-  #         var_label <- colnames(obj[col_num])
-  #       }
-  #     }
-  #     
-  #     # No dataframe found;
-  #     if (length(df_name) == 0) {
-  #       # Look for a variable; go up the chain of environments
-  #       for (.i in seq_along(allnames)) {
-  #         if (!cont)
-  #           break
-  #         for (.j in seq_along(sys.frames())) {
-  #           check <- exists(x = allnames[.i], where = .j, mode = 'character') ||
-  #             exists(x = allnames[.i], where = .j, mode = 'numeric')
-  #           if (isTRUE(check)) {
-  #             obj <- get(allnames[.i], pos = .j)
-  #             if (is.atomic(obj)) {
-  #               var_names <- allnames[.i]
-  #               var_label <- label(obj)
-  #               cont <- FALSE
-  #               break
-  #             }
-  #           }
-  #         }
-  #       }
-  #     }
-  #   }
-  #   
   
   output <- list(df_name = df_name,
                  df_label = df_label,
