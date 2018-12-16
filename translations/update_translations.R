9eb516303b50c17e936fc69e7f747a5a0b1f3494
Dominic Comtois <dominic.comtois@gmail.com>
2018-12-16 05:46:47 -0500

    set encoding to UTF-8 for translations' columns

--- translations/update_translations.R
+++ translations/update_translations.R
@@ -1,11 +1,18 @@
 # Update package translations
-tr <- read.csv2("translations.txt", strip.white = TRUE, stringsAsFactors = F,
+tr <- read.csv2("translations/translations.csv", strip.white = TRUE, stringsAsFactors = F,
                 fileEncoding = "UTF-8",)
 #apply(tr, c(1,2), conv_non_ascii)
 items <- tr$item
 tr <- t(as.matrix(tr[,-1:-2]))
 colnames(tr) <- items
 tr <- as.data.frame(tr, stringsAsFactors = FALSE)
-save(tr, file = "data/translations.RData")
 
+for (cname in colnames(tr)) {
+  Encoding(tr[,cname]) <- "UTF-8"
+}
+
+translations <- tr
+save(translations, file = "data/translations.RData")
+
+# Check that translations is in the package's environment
 #ls(loadNamespace("summarytools")) 
