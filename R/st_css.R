52803233b516559b656329e1c5811f7f6c988551
Dominic Comtois <dominic.comtois@gmail.com>
2019-01-25 12:43:37 -0500

    css update

--- R/st_css.R
+++ R/st_css.R
@@ -1,11 +1,16 @@
-#' Include \bold{summarytools}' \emph{css} Into Active Document
+#' Include \strong{summarytools}' \emph{css} Into Active Document
 #'
-#' Generates the \emph{css} needed by \bold{summarytools} in \emph{Rmarkdown} 
+#' Generates the \emph{css} needed by \strong{summarytools} in \emph{Rmarkdown} 
 #' documents.
 #'
-#' @param main Logical. Includes \emph{summarytools.css} file. \code{TRUE} by
+#' @param main Logical. Include \emph{summarytools.css} file. \code{TRUE} by
+#'   default. Affects only \strong{summarytools} objects.
+#' @param global Logical. Include the additional \emph{summarytools-global.css}
+#'   file, which affects all content in the document. Provides control over
+#'   objects that were not \emph{html-rendered}; in particular, table widths
+#'   and vertical alignment are modified to improve layout. \code{FALSE} by
 #'   default.
-#' @param bootstrap Logical. Includes \emph{bootstrap.min.css}. \code{FALSE}
+#' @param bootstrap Logical. Include \emph{bootstrap.min.css}. \code{FALSE}
 #'   by default.
 #' @param style.tag Logical. Includes the opening and closing \code{<style>}
 #'   tags. \code{TRUE} by default.
@@ -19,15 +24,15 @@
 #'   opts_chunk$set(results = "asis")
 #'   library(summarytools)
 #'   st_options(plain.ascii = FALSE, style = "rmarkdown")
-#'   include.css()
+#'   st_css()
 #'   ```
 #'   }
 #'   
 #' @keywords utilities
 #' @author Dominic Comtois, \email{dominic.comtois@@gmail.com}
 #' @export
-st_css <- function(main = TRUE, bootstrap = FALSE, 
-                          style.tag = TRUE, ...) {
+st_css <- function(main = TRUE, global = FALSE, bootstrap = FALSE, 
+                   style.tag = TRUE, ...) {
 
   output <- character()
   
@@ -38,6 +43,13 @@ st_css <- function(main = TRUE, bootstrap = FALSE,
   if (isTRUE(main)) {
     output %+=% readLines(system.file(package = "summarytools",
                                       "includes/stylesheets/summarytools.css"))
+  }
+
+  if (isTRUE(global)) {
+    output %+=% readLines(
+      system.file(package = "summarytools",
+                  "includes/stylesheets/summarytools-global.css")
+    )
   }
   
   if (isTRUE(bootstrap)) {
