a7e07c0e1845d359cc6b960cd55acc5955157042
Dominic Comtois <dominic.comtois@gmail.com>
2019-03-22 00:09:42 -0400

    update test ref files and make tests/ref/* non linguist-detectable

--- tests/00-Main.R
+++ tests/00-Main.R
@@ -119,27 +119,27 @@ compare_dirs <- function(lang) {
   }
 }
 
-eval_with_feedback(testfiles[1],  lang = "en", compare = FALSE) # parse-args
-eval_with_feedback(testfiles[2],  lang = "en", compare = FALSE) # freq
-eval_with_feedback(testfiles[3],  lang = "en", compare = FALSE) # ctable
-eval_with_feedback(testfiles[4],  lang = "en", compare = FALSE) # descr
-eval_with_feedback(testfiles[5],  lang = "en", compare = FALSE) # dfSummary
-eval_with_feedback(testfiles[6],  lang = "en", compare = FALSE) # overrides
-eval_with_feedback(testfiles[7],  lang = "en", compare = FALSE) # lapply
-eval_with_feedback(testfiles[8],  lang = "en", compare = FALSE) # with/by
-eval_with_feedback(testfiles[9],  lang = "en", compare = FALSE) # st_options
+lang <- c("en", "fr", "es", "pt", "tr")[4]
 
-compare_dirs('en')
+eval_with_feedback(testfiles[1],  lang = lang, compare = FALSE) # parse-args
+eval_with_feedback(testfiles[2],  lang = lang, compare = FALSE) # freq
+eval_with_feedback(testfiles[3],  lang = lang, compare = FALSE) # ctable
+eval_with_feedback(testfiles[4],  lang = lang, compare = FALSE) # descr
+eval_with_feedback(testfiles[5],  lang = lang, compare = FALSE) # dfSummary
+eval_with_feedback(testfiles[6],  lang = lang, compare = FALSE) # overrides
+eval_with_feedback(testfiles[7],  lang = lang, compare = FALSE) # lapply
+eval_with_feedback(testfiles[8],  lang = lang, compare = FALSE) # with/by
+eval_with_feedback(testfiles[9],  lang = lang, compare = FALSE) # st_options
 
-eval_with_feedback(testfiles[1],  lang = "fr", compare = FALSE) # parse-args
-eval_with_feedback(testfiles[2],  lang = "fr", compare = FALSE) # freq
-eval_with_feedback(testfiles[3],  lang = "fr", compare = FALSE) # ctable
-eval_with_feedback(testfiles[4],  lang = "fr", compare = FALSE) # descr
-eval_with_feedback(testfiles[5],  lang = "fr", compare = FALSE) # dfSummary
-eval_with_feedback(testfiles[6],  lang = "fr", compare = FALSE) # overrides
-eval_with_feedback(testfiles[7],  lang = "fr", compare = FALSE) # lapply
-eval_with_feedback(testfiles[8],  lang = "fr", compare = FALSE) # with/by
-eval_with_feedback(testfiles[9],  lang = "fr", compare = FALSE) # st_options
+compare_dirs(lang)
 
-compare_dirs('fr')
+eval_with_feedback(testfiles[1],  lang = "fr", compare = TRUE) # parse-args
+eval_with_feedback(testfiles[2],  lang = "fr", compare = TRUE) # freq
+eval_with_feedback(testfiles[3],  lang = "fr", compare = TRUE) # ctable
+eval_with_feedback(testfiles[4],  lang = "fr", compare = TRUE) # descr
+eval_with_feedback(testfiles[5],  lang = "fr", compare = TRUE) # dfSummary
+eval_with_feedback(testfiles[6],  lang = "fr", compare = TRUE) # overrides
+eval_with_feedback(testfiles[7],  lang = "fr", compare = TRUE) # lapply
+eval_with_feedback(testfiles[8],  lang = "fr", compare = TRUE) # with/by
+eval_with_feedback(testfiles[9],  lang = "fr", compare = TRUE) # st_options
 
