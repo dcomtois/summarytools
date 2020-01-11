57298619b3541f647fdaadf2cb025e99eb8fb4a6
Dominic Comtois <dominic.comtois@gmail.com>
2020-01-11 06:52:03 -0500

    prevent descr from assembling byst results into one table when more than one grouping variable is used

--- R/view.R
+++ R/view.R
@@ -76,8 +76,9 @@ view <- function(x,
     
   } else if (inherits(x = x, what = c("stby","by")) &&
              attr(x[[1]], "st_type") == "descr" &&
-             ((!attr(x[[1]], "data_info")$transposed && dim(x[[1]])[2] == 1) || 
-              ( attr(x[[1]], "data_info")$transposed && dim(x[[1]])[1] == 1))) {             
+             length(attr(x[[1]], "data_info")$by_var) == 1 &&
+             ((!attr(x[[1]], "data_info")$transposed && dim(x[[1]])[2] == 1) ||
+              ( attr(x[[1]], "data_info")$transposed && dim(x[[1]])[1] == 1))) {
 
     # Special case: descr by() objects with 1 variable -------------------------
     
