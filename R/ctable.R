de220b0c58ef1be136bb99a2ef70ef3daf228ed1
Dominic Comtois <dominic.comtois@gmail.com>
2022-11-06 22:51:56 -0500

    Revert "fixes and apply roxygen2"
    This reverts commit 3dd1e4c0d9bb5992d87245118c7d4c862f4c969a.

--- R/ctable.R
+++ R/ctable.R
@@ -114,14 +114,12 @@ ctable <- function(x, ...){
 } 
 
 ## S3 method for class 'formula'
-
 #' @method ctable formula
-#' @rdname ctable
 #' @export
 #' @importFrom stats terms
 ctable.formula <- function(x, data, ...){
-  vars <- all.vars(x)
-  resp <- attr(terms(x), "response")
+  vars <- all.vars(formula)
+  resp <- attr(terms(formula), "response")
   if(length(vars) > 2 || resp == 0){
     stop("Formula has not the form y ~ x")
   }
@@ -131,9 +129,7 @@ ctable.formula <- function(x, data, ...){
 }
 
 ## Default S3 method
-
 #' @method ctable default
-#' @rdname ctable
 #' @export
 #' @importFrom stats addmargins na.omit chisq.test qnorm
 ctable.default <- function(x, 
